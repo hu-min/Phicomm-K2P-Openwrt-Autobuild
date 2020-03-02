03e105952708e375b1dd23237b49ce0ea463ebc4
none <hu-min@users.noreply.github.com>
2020-03-02 21:12:15 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,6 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+#默认开启wifi
+#sed -i 's/set wireless.radio${devidx}.disabled=0/set wireless.radio${devidx}.disabled=1/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i 's/set wireless.radio${devidx}.disabled=0/set wireless.radio${devidx}.disabled=1/g' package/base-files/files/lib/functions/uci-defaults.sh
